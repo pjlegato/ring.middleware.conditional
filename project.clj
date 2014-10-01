e442045ae2c6482eef50118055619c301748c83e
Paul Legato <pjlegato@gmail.com>
2014-10-01 13:48:42 -0700

    Initial commit

---
+++ project.clj
@@ -0,0 +1,8 @@
+(defproject ring.middleware.conditional "0.1.1-SNAPSHOT"
+  :description "Adds arbitrary conditions to the Ring middleware stack"
+  :url "http://github.com/pjlegato/ring.middleware.conditional"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.6.0"]
+                 [ring "1.3.1"]]
+  :profiles {:dev {:dependencies [[ring-mock "0.1.5"]]}})
