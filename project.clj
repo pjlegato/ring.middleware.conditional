686349cf9744f261c9d4e4f1a0629ca1968579e5
Matt Kipps <matkips@gmail.com>
2015-03-30 21:59:04 -0700

    don't include ring directly in dependencies

--- project.clj
+++ project.clj
@@ -3,6 +3,6 @@
   :url "http://github.com/pjlegato/ring.middleware.conditional"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.6.0"]
-                 [ring "1.3.1"]]
-  :profiles {:dev {:dependencies [[ring-mock "0.1.5"]]}})
+  :dependencies [[org.clojure/clojure "1.6.0"]]
+  :profiles {:dev {:dependencies [[ring "1.3.1"]
+                                  [ring-mock "0.1.5"]]}})
